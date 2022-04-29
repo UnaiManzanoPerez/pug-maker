2698404bb3a6d2fc4204a1ff6b5fb5790e6ab7f3
UnaiManzanoPerez <79275029+UnaiManzanoPerez@users.noreply.github.com>
2022-04-29 20:43:14 +0200

    v1

--- script.js
+++ script.js
@@ -1,26 +1,107 @@
-let tank = []
-let dps = []
-let support = []
+let tankArray = []
+let dpsArray = []
+let supportArray = []
 
-function addTank(){
+let okTank = false;
+let okDPS = false;
+let okSupport = false;
+
+function addTank() {
     let tankToAdd = document.getElementById("inputTank").value
-    console.log(tankToAdd)
+    tankArray.push(tankToAdd)
+
     let tankList = document.getElementById("tankList")
+    tankList.innerHTML += tankToAdd + "<br>"
 
-    tankList.innerHTML += tankToAdd +"<br>"
-    document.getElementById("inputTank").value=""
+    document.getElementById("inputTank").value = ""
+    enoughPlayers()
 }
 
-function addDPS(){
+function addDPS() {
     let dpsToAdd = document.getElementById("inputDPS").value
+    dpsArray.push(dpsToAdd)
+
     let dpsList = document.getElementById("dpsList")
+    dpsList.innerHTML += dpsToAdd + "<br>"
 
-    dpsList.innerHTML += dpsToAdd +"<br>"
+    document.getElementById("inputDPS").value = ""
+    enoughPlayers()
 }
 
-function addSupport(){
+function addSupport() {
     let supportToAdd = document.getElementById("inputSupport").value
-    let supportList = document.getElementById("supportList")
+    supportArray.push(supportToAdd)
 
-    supportList.innerHTML += supportToAdd +"<br>"
+    let supportList = document.getElementById("supportList")
+    supportList.innerHTML += supportToAdd + "<br>"
+
+    document.getElementById("inputSupport").value = ""
+    enoughPlayers()
 }
+
+function enoughPlayers(){
+    if (tankArray.length >= 2) {
+        okTank = true;
+    }
+
+    if (dpsArray.length >= 4) {
+        okDPS = true;
+    }
+
+    if (supportArray.length >= 4) {
+        okSupport = true;
+    }
+
+    if (okTank == true && okDPS == true && okSupport == true) {
+        document.getElementById("makeTeamButton").disabled = false
+    }
+}
+
+function makeTeam() {
+    // TEAM 1
+    let divTeam1 = document.getElementById("team1")
+
+    let randomTank1A = tankArray[Math.floor(Math.random() * tankArray.length)]
+    divTeam1.innerHTML+=randomTank1A + "<br>"
+    tankArray.splice(randomTank1A, 1)
+
+    let randomDPS1A = dpsArray[Math.floor(Math.random() * dpsArray.length)]
+    divTeam1.innerHTML+=randomDPS1A + "<br>"
+    dpsArray.splice(randomDPS1A, 1)
+
+    let randomDPS2A = dpsArray[Math.floor(Math.random() * dpsArray.length)]
+    divTeam1.innerHTML+=randomDPS2A + "<br>"
+    dpsArray.splice(randomDPS2A, 1)
+
+    let randomSupport1A = supportArray[Math.floor(Math.random() * supportArray.length)]
+    divTeam1.innerHTML+=randomSupport1A + "<br>"
+    supportArray.splice(randomSupport1A, 1)
+
+    let randomSupport2A = supportArray[Math.floor(Math.random() * supportArray.length)]
+    divTeam1.innerHTML+=randomSupport2A + "<br>"
+    supportArray.splice(randomSupport2A, 1)
+
+    // TEAM 2
+    let divTeam2 = document.getElementById("team2")
+
+    let randomTank1B = tankArray[Math.floor(Math.random() * tankArray.length)]
+    divTeam2.innerHTML+=randomTank1B + "<br>"
+    tankArray.splice(randomTank1B, 1)
+
+    let randomDPS1B = dpsArray[Math.floor(Math.random() * dpsArray.length)]
+    divTeam2.innerHTML+=randomDPS1B + "<br>"
+    dpsArray.splice(randomDPS1B, 1)
+
+    let randomDPS2B = dpsArray[Math.floor(Math.random() * dpsArray.length)]
+    divTeam2.innerHTML+=randomDPS2B + "<br>"
+    dpsArray.splice(randomDPS2B, 1)
+
+    let randomSupport1B = supportArray[Math.floor(Math.random() * supportArray.length)]
+    divTeam2.innerHTML+=randomSupport1B + "<br>"
+    supportArray.splice(randomSupport1B, 1)
+
+    let randomSupport2B = supportArray[Math.floor(Math.random() * supportArray.length)]
+    divTeam2.innerHTML+=randomSupport2B + "<br>"
+    supportArray.splice(randomSupport2B, 1)
+}
+
