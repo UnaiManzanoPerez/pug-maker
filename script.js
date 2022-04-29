79b538c55c01ca478ea9e774437705a51dc0c627
UnaiManzanoPerez <79275029+UnaiManzanoPerez@users.noreply.github.com>
2022-04-29 21:00:54 +0200

    nightmode

--- script.js
+++ script.js
@@ -105,3 +105,21 @@ function makeTeam() {
     supportArray.splice(randomSupport2B, 1)
 }
 
+
+function darkMode(){
+    const css = `html {
+        -webkit-filter: invert(100%);
+        -moz-filter: invert(100%);
+        -o-filter: invert(100%);
+        -ms-filter: invert(100%);
+      }`
+      const head = document.head
+      const style = document.createElement('style')
+      style.type = 'text/css';
+      if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+      } else {
+        style.appendChild(document.createTextNode(css));
+      }
+      head.appendChild(style);
+}
